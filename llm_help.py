da81331a590e0a83437ebcfadca920814aa97648
Aishwarya Kashyap <aishwaryakashy@gmail.com>
2026-04-20 15:35:45 +0530

    For hosting again

--- llm_help.py
+++ llm_help.py
@@ -2,7 +2,11 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 import os
 
-load_dotenv() 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
 
 llm=ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model="llama-3.1-8b-instant")
 
